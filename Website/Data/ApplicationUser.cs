db990ddc46023f6cb3bb8b71b6ef787e8e358390
Z1chi23 <afifsofian.as@gmail.com>
2024-05-18 23:56:17 +0800

    Add a few codes

--- Website/Data/ApplicationUser.cs
+++ Website/Data/ApplicationUser.cs
@@ -5,6 +5,6 @@ namespace Website.Areas.Identity.Data
     public class ApplicationUser : IdentityUser
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; } // Change the type from object to string
+        public string LastName { get; set; }
     }
 }
